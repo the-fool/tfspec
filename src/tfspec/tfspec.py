6b6c8689025343e92943da0fda2cd081dad09c3b
thomas ruble <sketchbang@gmail.com>
2020-06-15 21:31:32 +0000

    fix typos

--- src/tfspec/tfspec.py
+++ src/tfspec/tfspec.py
@@ -1,19 +1,3 @@
-class Optional(Spec):
-    """Marker for an optional part of the validation Schema."""
-
-    _MARKER = object()
-
-    def __init__(self, *args, **kwargs):
-        super(Optional, self).__init__(*args, **kwargs)
-        self.key = str(self._schema)
-
-    def __hash__(self):
-        return hash(self._spec)
-
-    def __eq__(self, other):
-        return self.__class__ is other.__class__ and self._spec == other._spec
-
-
 class Spec:
     def __init__(self, spec):
         self._spec = spec
@@ -30,3 +14,19 @@ class Spec:
     def _is_optional_type(s):
         """Return True if the given key is optional (does not have to be found)"""
         return isinstance(s, Optional)
+
+
+class Optional(Spec):
+    """Marker for an optional part of the validation Schema."""
+
+    _MARKER = object()
+
+    def __init__(self, *args, **kwargs):
+        super(Optional, self).__init__(*args, **kwargs)
+        self.key = str(self._schema)
+
+    def __hash__(self):
+        return hash(self._spec)
+
+    def __eq__(self, other):
+        return self.__class__ is other.__class__ and self._spec == other._spec
